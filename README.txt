71890357d8e5744c6055b51be261a98240515648
Abhinav Umat <46773288+Abhinavumat95@users.noreply.github.com>
2023-01-15 13:20:33 -0500

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,18 @@
+You can run the code through terminal. Open your terminal and navigate to the directory where git_status.py file is located.
+
+On terminal type "python3 git_status.py" to run the code.
+
+The code will first ask you whether you want to clone a repository to the local machine or not. Press 0 for No and 1 for Yes.
+
+If you pressed 0, then it will ask you to enter the path where the repository is stored in your local machine.
+
+If you pressed 1, then it will ask you to enter the path for your repository and the path of your local machine where you want to clone the repository.
+
+Then it will print all the results for that repository.
+
+An example of the results is as follows:
+
+active branch:  main
+local changes:  False
+recent commit:  False
+blame Rufus:  False
---
+++ git_status.py
@@ -0,0 +1,67 @@
+# Importing necessary packages and libraries
+from git import Repo
+import time
+from datetime import date, datetime
+
+# Taking input from the user
+r = int(input("Do you want to clone the repository in local machine press 0 for No or 1 for yes. "))
+if (r == 0):
+    path2 = input("Enter the path where the repository is stored in local machine: ")
+    repo = Repo(path2)
+elif (r == 1):
+    path1 = input("Enter path for your repository: ")
+    path2 = input("Enter the path for local machine where you want to clone the repository: ")
+    Repo.clone_from(path1, path2)
+    repo = Repo(path2)
+else:
+    raise Exception("Sorry, please Enter either 0 or 1.")
+
+
+# Getting the active branch of repository
+active_branch = repo.active_branch
+
+# Getting the info for the latest commit
+commit = repo.head.commit
+
+# Getting the date for the last commit
+authored_date = commit.authored_date
+d1 = time.strftime("%Y/%m/%d", time.gmtime(authored_date))
+
+# Getting the date for the present day
+t = date.today()
+d2 = t.strftime("%Y/%m/%d")
+
+# Getting the difference between present date and last commit date 
+date1 = datetime.strptime(d1, "%Y/%m/%d")
+date2 = datetime.strptime(d2, "%Y/%m/%d")
+delta = date2 - date1
+
+
+# Checking if last commit was authored in last week
+if (delta.days > 7):
+    recent_commit = False
+else:
+    recent_commit = True
+
+# Checking if last commit was authored by Rufus
+if (commit.author.name == "Rufus"):
+    blame_rufus = True
+else:
+    blame_rufus = False
+
+
+# Getting the list of all modified files
+modified_files = [ item.a_path for item in repo.index.diff(None) ]
+
+# Checking if any files were modified
+if (modified_files):
+    local_changes = True
+else:
+    local_changes = False
+
+
+# Printing the results 
+print("active branch: ",active_branch)
+print("local changes: ",local_changes)
+print("recent commit: ",recent_commit)
+print("blame Rufus: ",blame_rufus)
